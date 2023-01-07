f9693bf65ff6070da25febd0f27a1671c4cbfd64
mengliiii <menglivy@hotmail.com>
2023-01-07 16:11:48 -0500

    mina

---
+++ src/package1/Mina.java
@@ -0,0 +1,10 @@
+package package1;
+
+public class Mina {
+    public static void main(String[] args) {
+        //i fork the project from cydeo account
+        //clone it from my github
+        //from my local i make changes
+        //push
+    }
+}
